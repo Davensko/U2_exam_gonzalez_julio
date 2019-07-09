d802864462af07e6d5c66dd3be0b737b4bedb9c3
Davensko <al1609053@upy.edu.mx>
2019-07-09 02:05:35 -0500

    Working on the code

--- controllers/u3_exam_gonzalez_julio/build/release/u3_exam_gonzalez_julio.d
+++ controllers/u3_exam_gonzalez_julio/build/release/u3_exam_gonzalez_julio.d
@@ -3,4 +3,6 @@ build/release/u3_exam_gonzalez_julio.o: u3_exam_gonzalez_julio.c \
  /usr/local/webots/include/controller/c/webots/types.h \
  /usr/local/webots/include/controller/c/webots/nodes.h \
  /usr/local/webots/include/controller/c/webots/motor.h \
- /usr/local/webots/include/controller/c/webots/keyboard.h
+ /usr/local/webots/include/controller/c/webots/keyboard.h \
+ /usr/local/webots/include/controller/c/webots/position_sensor.h \
+ /usr/local/webots/include/controller/c/webots/distance_sensor.h
--- controllers/u3_exam_gonzalez_julio/u3_exam_gonzalez_julio.c
+++ controllers/u3_exam_gonzalez_julio/u3_exam_gonzalez_julio.c
@@ -6,189 +6,122 @@
  * Modifications:
  */
 
-/*
- * You may need to add include files like <webots/distance_sensor.h> or
- * <webots/differential_wheels.h>, etc.
- */
 #include <webots/robot.h>
 #include <webots/motor.h>
 #include <webots/keyboard.h>
-//#include <webots/position_sensor.h>
-//#include <webots/distance_sensor.h>
+#include <webots/position_sensor.h>
+#include <webots/distance_sensor.h>
 
 #include <stdio.h>
 
-/*
- * You may want to add macros here.
- */
 #define TIME_STEP 64
 //#define pi 3.1416
 
-/*
- * This is the main program.
- * The arguments of the main function can be specified by the
- * "controllerArgs" field of the Robot node
- */
+ 
+double sn; 
+double sn2; 
+double dis;
+double dis2; 
+double encoder; 
+int paraR, paraL = 0;
+float Comparador = 0;
+ 
 int main(int argc, char **argv)
 {
-  /* necessary to initialize webots stuff */
+ 
   wb_robot_init();
-  wb_keyboard_enable(TIME_STEP);
-  
-  double vel = 0;
+ 
+  double vel = 2.0;
 
-  /*
-   * You should declare here WbDeviceTag variables for storing
-   * robot devices like this:
-   *  WbDeviceTag my_sensor = wb_robot_get_device("my_sensor");
-   *  WbDeviceTag my_actuator = wb_robot_get_device("my_actuator");
-   */
 
      WbDeviceTag wheel1 = wb_robot_get_device("Wheel_1");
      WbDeviceTag wheel2 = wb_robot_get_device("Wheel_2");
      WbDeviceTag wheel3 = wb_robot_get_device("Wheel_3");
-   
-  /* main loop
-   * Perform simulation steps of TIME_STEP milliseconds
-   * and leave the loop when the simulation is over
-   */
-   
-     wb_motor_set_position(wheel1, INFINITY);
-     wb_motor_set_velocity(wheel1, vel);   
-     wb_motor_set_position(wheel2, INFINITY);
-     wb_motor_set_velocity(wheel2, vel);
-     wb_motor_set_position(wheel3, INFINITY);
-     wb_motor_set_velocity(wheel3, vel);
-   
+     
+     WbDeviceTag sen1= wb_robot_get_device("sen_1");
+     WbDeviceTag sen2 = wb_robot_get_device("sen_2");
+     
+     wb_distance_sensor_enable(sen1,TIME_STEP);
+     wb_distance_sensor_enable(sen2,TIME_STEP);
+     
+     WbDeviceTag enco_1 = wb_robot_get_device("encoder_1");
+     WbDeviceTag enco_2 = wb_robot_get_device("encoder_2");
+     WbDeviceTag enco_3 = wb_robot_get_device("encoder_3");
+     
+     wb_position_sensor_enable(enco_1, TIME_STEP);
+     wb_position_sensor_enable(enco_2, TIME_STEP);
+     wb_position_sensor_enable(enco_3, TIME_STEP);
+     
+        
    
   while (wb_robot_step(TIME_STEP) != -1) {
-
-
-     int key = wb_keyboard_get_key(); 
-     int stop; 
-    /*
-     * Read the sensors :
-     * Enter here functions to read sensor data, like:
-     *  double val = wb_distance_sensor_get_value(my_sensor);
-     */
-     if (key==WB_KEYBOARD_UP){
-     
-         stop=key; 
-         vel = 2.0; 
+   
     
-         wb_motor_set_position(wheel1, INFINITY);
-         wb_motor_set_velocity(wheel1, 0);
-         wb_motor_set_position(wheel2, INFINITY);
-         wb_motor_set_velocity(wheel2, -vel);
-         wb_motor_set_position(wheel3, INFINITY);
-         wb_motor_set_velocity(wheel3, vel);
-     }
+       wb_motor_set_position(wheel1, INFINITY);
+       wb_motor_set_velocity(wheel1, 0);
+       wb_motor_set_position(wheel2, INFINITY);
+       wb_motor_set_velocity(wheel2, -vel);
+       wb_motor_set_position(wheel3, INFINITY);
+       wb_motor_set_velocity(wheel3, vel);
      
-     if (stop != key){
-      
-         wb_motor_set_position(wheel1, INFINITY);
-         wb_motor_set_velocity(wheel1, 0);
-         wb_motor_set_position(wheel2, INFINITY);
-         wb_motor_set_velocity(wheel2, 0);
-         wb_motor_set_position(wheel3, INFINITY);
-         wb_motor_set_velocity(wheel3, 0);
-     }  
+     sn = wb_distance_sensor_get_value(sen1);
+     dis = (sn*0.2)/65535;
      
-     if (key==WB_KEYBOARD_DOWN){
+     sn2 = wb_distance_sensor_get_value(sen2);
+     dis2 = (sn2*0.2)/65535;
      
-         vel = 2.0; 
-           
-         wb_motor_set_position(wheel1, INFINITY);
-         wb_motor_set_velocity(wheel1, 0);
-         wb_motor_set_position(wheel2, INFINITY);
-         wb_motor_set_velocity(wheel2, vel);
-         wb_motor_set_position(wheel3, INFINITY);
-         wb_motor_set_velocity(wheel3, -vel);
-     }
-     /*
-     if (key==WB_KEYBOARD_LEFT){
+     encoder = wb_position_sensor_get_value(enco_3);
      
-         vel= 80; 
-     
-         wb_motor_set_position(wheel1, INFINITY);
-         wb_motor_set_velocity(wheel1, vel);
-         wb_motor_set_position(wheel2, INFINITY);
-         wb_motor_set_velocity(wheel2, vel);
-         wb_motor_set_position(wheel3, INFINITY);
-         wb_motor_set_velocity(wheel3, 0);
-     }
-     
-     if (key==WB_KEYBOARD_RIGHT){
-     
-         vel= 80; 
-     
-         wb_motor_set_position(wheel1, INFINITY);
-         wb_motor_set_velocity(wheel1, -vel);
-         wb_motor_set_position(wheel2, INFINITY);
-         wb_motor_set_velocity(wheel2, 0);
-         wb_motor_set_position(wheel3, INFINITY);
-         wb_motor_set_velocity(wheel3, -vel);
-     }
-     */
-     if (key==WB_KEYBOARD_PAGEUP){
-     
-         vel= 2.0; 
-     
-         wb_motor_set_position(wheel1, INFINITY);
-         wb_motor_set_velocity(wheel1, -vel);
-         wb_motor_set_position(wheel2, INFINITY);
-         wb_motor_set_velocity(wheel2, -vel);
-         wb_motor_set_position(wheel3, INFINITY);
-         wb_motor_set_velocity(wheel3, 0);
-     }
-     
-     if (key==WB_KEYBOARD_PAGEDOWN){
-     
-         vel= 2.0; 
-     
-         wb_motor_set_position(wheel1, INFINITY);
-         wb_motor_set_velocity(wheel1, vel);
-         wb_motor_set_position(wheel2, INFINITY);
-         wb_motor_set_velocity(wheel2, 0);
-         wb_motor_set_position(wheel3, INFINITY);
-         wb_motor_set_velocity(wheel3, vel);
-     }
-     
-     if (key==WB_KEYBOARD_LEFT){
-     
-         vel= 2.0; 
-     
-         wb_motor_set_position(wheel1, INFINITY);
-         wb_motor_set_velocity(wheel1, -vel);
-         wb_motor_set_position(wheel2, INFINITY);
-         wb_motor_set_velocity(wheel2, -vel);
-         wb_motor_set_position(wheel3, INFINITY);
-         wb_motor_set_velocity(wheel3, -vel);
-     }
-     
-     if (key==WB_KEYBOARD_RIGHT){
-     
-         vel= 2.0; 
-     
-         wb_motor_set_position(wheel1, INFINITY);
-         wb_motor_set_velocity(wheel1, vel);
-         wb_motor_set_position(wheel2, INFINITY);
-         wb_motor_set_velocity(wheel2, vel);
-         wb_motor_set_position(wheel3, INFINITY);
-         wb_motor_set_velocity(wheel3, vel);
+     if((dis <= 0.17 && dis>dis2) && paraR == 0){
+       Comparador = encoder + (2.35619); //.75 = 45 degrees to the left
+       paraR = 1;
      }
 
-    /* Process sensor data here */
+     if(paraR == 1){
 
-    /*
-     * Enter here functions to send actuator commands, like:
-     * wb_differential_wheels_set_speed(100.0,100.0);
-     */
+     if(encoder <= Comparador){
+       wb_motor_set_velocity(wheel1, 5);
+       wb_motor_set_velocity(wheel2, 5);
+       wb_motor_set_velocity(wheel3, 5);
+     }
+     else{
+       wb_motor_set_velocity(wheel1, 0);
+       wb_motor_set_velocity(wheel2, 0);
+       wb_motor_set_velocity(wheel3, 0);
+       paraR = 0;
+     }       
+     }
+
+     if((dis2 <= 0.17 && dis<dis2) && paraL == 0){
+       Comparador = encoder - (2.35619); //.75 = 45 degrees to the left
+       paraL = 1;
+     }
+
+     if(paraL == 1){
+
+     if(encoder >= Comparador){
+       wb_motor_set_velocity(wheel1, -5);
+       wb_motor_set_velocity(wheel2, -5);
+       wb_motor_set_velocity(wheel3, -5);
+     }
+     else{
+       wb_motor_set_velocity(wheel1, 0);
+       wb_motor_set_velocity(wheel2, 0);
+       wb_motor_set_velocity(wheel3, 0);
+       paraL = 0;
+     }   
+     
+     }     
+    
+    
+     printf("sensor: %f\n", dis);
+     printf("sensor2: %f\n", dis2);
+     printf("Encoder: %f\n", encoder);
+     printf("flag: %f\n", Comparador);
+     
+     
   };
 
-  /* Enter your cleanup code here */
-
-  /* This is necessary to cleanup webots resources */
   wb_robot_cleanup();
 
   return 0;
