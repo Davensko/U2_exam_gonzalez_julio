ae93a8910129563c902117787625f730e2ce4fb5
Davensko <al1609053@upy.edu.mx>
2019-07-09 02:37:13 -0500

    Nomenclature of the code fixed

--- controllers/u3_exam_gonzalez_julio/u3_exam_gonzalez_julio.c
+++ controllers/u3_exam_gonzalez_julio/u3_exam_gonzalez_julio.c
@@ -19,107 +19,118 @@
 
  
 double sn; 
-double sn2; 
+double sn_2; 
 double dis;
-double dis2; 
-double encoder; 
-int paraR, paraL = 0;
-float Comparador = 0;
+double dis_2; 
+double encoder;
+double vel = 2.0;
  
-int main(int argc, char **argv)
-{
+int stop_r, stop_l = 0;
+
+float compare = 0;
+ 
+int main(int argc, char **argv){
  
   wb_robot_init();
  
-  double vel = 2.0;
-
-
-     WbDeviceTag wheel1 = wb_robot_get_device("Wheel_1");
-     WbDeviceTag wheel2 = wb_robot_get_device("Wheel_2");
-     WbDeviceTag wheel3 = wb_robot_get_device("Wheel_3");
+  WbDeviceTag wheel_1 = wb_robot_get_device("Wheel_1");
+  WbDeviceTag wheel_2 = wb_robot_get_device("Wheel_2");
+  WbDeviceTag wheel_3 = wb_robot_get_device("Wheel_3");
      
-     WbDeviceTag sen1= wb_robot_get_device("sen_1");
-     WbDeviceTag sen2 = wb_robot_get_device("sen_2");
+  WbDeviceTag sen_1= wb_robot_get_device("sen_1");
+  WbDeviceTag sen_2 = wb_robot_get_device("sen_2");
      
-     wb_distance_sensor_enable(sen1,TIME_STEP);
-     wb_distance_sensor_enable(sen2,TIME_STEP);
+  wb_distance_sensor_enable(sen_1,TIME_STEP);
+  wb_distance_sensor_enable(sen_2,TIME_STEP);
      
-     WbDeviceTag enco_1 = wb_robot_get_device("encoder_1");
-     WbDeviceTag enco_2 = wb_robot_get_device("encoder_2");
-     WbDeviceTag enco_3 = wb_robot_get_device("encoder_3");
+  WbDeviceTag enco_1 = wb_robot_get_device("encoder_1");
+  WbDeviceTag enco_2 = wb_robot_get_device("encoder_2");
+  WbDeviceTag enco_3 = wb_robot_get_device("encoder_3");
      
-     wb_position_sensor_enable(enco_1, TIME_STEP);
-     wb_position_sensor_enable(enco_2, TIME_STEP);
-     wb_position_sensor_enable(enco_3, TIME_STEP);
+  wb_position_sensor_enable(enco_1, TIME_STEP);
+  wb_position_sensor_enable(enco_2, TIME_STEP);
+  wb_position_sensor_enable(enco_3, TIME_STEP);
      
         
    
   while (wb_robot_step(TIME_STEP) != -1) {
    
     
-       wb_motor_set_position(wheel1, INFINITY);
-       wb_motor_set_velocity(wheel1, 0);
-       wb_motor_set_position(wheel2, INFINITY);
-       wb_motor_set_velocity(wheel2, -vel);
-       wb_motor_set_position(wheel3, INFINITY);
-       wb_motor_set_velocity(wheel3, vel);
+    wb_motor_set_position(wheel_1, INFINITY);
+    wb_motor_set_velocity(wheel_1, 0);
+    wb_motor_set_position(wheel_2, INFINITY);
+    wb_motor_set_velocity(wheel_2, -vel);
+    wb_motor_set_position(wheel_3, INFINITY);
+    wb_motor_set_velocity(wheel_3, vel);
      
-     sn = wb_distance_sensor_get_value(sen1);
-     dis = (sn*0.2)/65535;
+    sn = wb_distance_sensor_get_value(sen_1);
+    dis = (sn*0.2)/65535;
      
-     sn2 = wb_distance_sensor_get_value(sen2);
-     dis2 = (sn2*0.2)/65535;
+    sn_2 = wb_distance_sensor_get_value(sen_2);
+    dis_2 = (sn_2*0.2)/65535;
      
-     encoder = wb_position_sensor_get_value(enco_3);
+    encoder = wb_position_sensor_get_value(enco_3);
      
-     if((dis <= 0.17 && dis>dis2) && paraR == 0){
-       Comparador = encoder + (2.35619); //.75 = 45 degrees to the left
-       paraR = 1;
-     }
-
-     if(paraR == 1){
-
-     if(encoder <= Comparador){
-       wb_motor_set_velocity(wheel1, 5);
-       wb_motor_set_velocity(wheel2, 5);
-       wb_motor_set_velocity(wheel3, 5);
-     }
-     else{
-       wb_motor_set_velocity(wheel1, 0);
-       wb_motor_set_velocity(wheel2, 0);
-       wb_motor_set_velocity(wheel3, 0);
-       paraR = 0;
-     }       
-     }
-
-     if((dis2 <= 0.17 && dis<dis2) && paraL == 0){
-       Comparador = encoder - (2.35619); //.75 = 45 degrees to the left
-       paraL = 1;
-     }
-
-     if(paraL == 1){
-
-     if(encoder >= Comparador){
-       wb_motor_set_velocity(wheel1, -5);
-       wb_motor_set_velocity(wheel2, -5);
-       wb_motor_set_velocity(wheel3, -5);
-     }
-     else{
-       wb_motor_set_velocity(wheel1, 0);
-       wb_motor_set_velocity(wheel2, 0);
-       wb_motor_set_velocity(wheel3, 0);
-       paraL = 0;
-     }   
-     
-     }     
+    if((dis <= 0.17 && dis>dis_2) && stop_r == 0){
     
+      compare = encoder + (2.35619);   // 2.35619 rad/s = 135 degrees
+      stop_r = 1;
+      
+    }
+
+    if(stop_r == 1){
+
+      if(encoder <= compare){
+      
+        wb_motor_set_velocity(wheel_1, 5);
+        wb_motor_set_velocity(wheel_2, 5);
+        wb_motor_set_velocity(wheel_3, 5);
+        
+      }
+     
+      else{
+        wb_motor_set_velocity(wheel_1, 0);
+        wb_motor_set_velocity(wheel_2, 0);
+        wb_motor_set_velocity(wheel_3, 0);
+        
+        stop_r = 0;
+      }
+             
+    }
+
+    if((dis_2 <= 0.17 && dis<dis_2) && stop_l == 0){
     
-     printf("sensor: %f\n", dis);
-     printf("sensor2: %f\n", dis2);
-     printf("Encoder: %f\n", encoder);
-     printf("flag: %f\n", Comparador);
+      compare = encoder - (2.35619); 
+      stop_l = 1;
+      
+    }
+
+    if(stop_l == 1){
+
+      if(encoder >= compare){
+      
+        wb_motor_set_velocity(wheel_1, -5);
+        wb_motor_set_velocity(wheel_2, -5);
+        wb_motor_set_velocity(wheel_3, -5);
+      }
+      
+      else{
+      
+        wb_motor_set_velocity(wheel_1, 0);
+        wb_motor_set_velocity(wheel_2, 0);
+        wb_motor_set_velocity(wheel_3, 0);
+        
+        stop_l = 0;
+        
+      }   
      
-     
+    }     
+    
+    printf("sensor: %f\n", dis);
+    printf("sensor2: %f\n", dis_2);
+    printf("Encoder: %f\n", encoder);
+    printf("flag: %f\n", compare);
+          
   };
 
   wb_robot_cleanup();
